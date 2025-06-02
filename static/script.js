0e5d3989647bd093428d75315cb2d97c08d4406e
HaeengIn <haeengin@gmail.com>
2025-06-02 19:11:37 +0900

    add fade-up transition

---
+++ static/script.js
@@ -0,0 +1,25 @@
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const fadeEls = document.querySelectorAll('.fade-up');
+    const observer = new IntersectionObserver(
+        (entries, obs) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                    obs.unobserve(entry.target);
+                }
+            });
+        },
+        {
+            threshold: 0.1
+        }
+    );
+    fadeEls.forEach(el => observer.observe(el));
+});
+
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
+});
