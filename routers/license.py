1e1e1ad1cb22f3684a5ed203811484b12097d95b
HaeengIn <haeengin@gmail.com>
2026-05-13 02:13:21 +0900

    Add license page for SHIRO

--- routers/license.py
+++ routers/license.py
@@ -16,4 +16,14 @@ async def wppengine(request: Request):
 @license_router.get("/wppengine/shiro")
 async def wppengine_pages(request: Request):
     from supabase_client import supabase
-    return templates.TemplateResponse(request=request, name="license/wppengine/shiro.html")
+    try:
+        response = (supabase.table("shiro_verification").select("*").execute())
+        rows = response.data
+
+        status_map = {0: "Disallowed", 1: "Allowed", 2: "Partially Allowed"}
+        for row in rows:
+            row["verify_status_text"] = status_map.get(row["verify_status"], "Unknown") #type: ignore
+        
+        return templates.TemplateResponse(request=request, context={"verifications": rows}, name="license/wppengine/shiro.html")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
