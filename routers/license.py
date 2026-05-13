fde6476eea90cc39c2a4c3e0e19806fc1da3bc46
HaeengIn <haeengin@gmail.com>
2026-05-13 04:42:41 +0000

    Apply Black Formatter

--- routers/license.py
+++ routers/license.py
@@ -3,27 +3,36 @@ from templates_config import templates
 
 license_router = APIRouter(prefix="/license", redirect_slashes=True)
 
+
 @license_router.get("")
 async def index(request: Request):
     return templates.TemplateResponse(request=request, name="license/index.html")
 
+
 @license_router.get("/wppengine")
 async def wppengine(request: Request):
-    return templates.TemplateResponse(request=request, name="license/wppengine/index.html")
+    return templates.TemplateResponse(
+        request=request, name="license/wppengine/index.html"
+    )
+
 
 @license_router.get("/")
-
 @license_router.get("/wppengine/shiro")
 async def wppengine_pages(request: Request):
     from supabase_client import supabase
+
     try:
-        response = (supabase.table("shiro_verification").select("*").execute())
+        response = supabase.table("shiro_verification").select("*").execute()
         rows = response.data
 
         status_map = {0: "Disallowed", 1: "Allowed", 2: "Partially Allowed"}
         for row in rows:
-            row["verify_status_text"] = status_map.get(row["verify_status"], "Unknown") #type: ignore
-        
-        return templates.TemplateResponse(request=request, context={"verifications": rows}, name="license/wppengine/shiro.html")
+            row["verify_status_text"] = status_map.get(row["verify_status"], "Unknown")  # type: ignore
+
+        return templates.TemplateResponse(
+            request=request,
+            context={"verifications": rows},
+            name="license/wppengine/shiro.html",
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
