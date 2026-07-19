7a3ab1e809832105c9125a7f044c9eabe69cde94
HaeengIn <haeengin@gmail.com>
2026-07-19 17:29:38 +0900

    Add adofai router

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from templates_config import templates
 from routers.license import license_router
 from routers.download_installer import download_installer_router
 from routers.about import about_router
+from routers.adofai import adofai_router
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -32,3 +33,4 @@ async def index(request: Request):
 app.include_router(license_router)
 app.include_router(download_installer_router)
 app.include_router(about_router)
+app.include_router(adofai_router)
---
+++ routers/adofai.py
@@ -0,0 +1,41 @@
+import json
+
+from fastapi import APIRouter, Request
+from templates_config import templates
+
+adofai_router = APIRouter(prefix="/adofai", redirect_slashes=True)
+
+with open("static/db/data.json", "r") as f:
+    data = json.load(f)
+    data = data["adofai"].values()
+
+
+@adofai_router.get("")
+async def index(request: Request):
+    title = ("ADOFAI - NaGNae",)
+    h1 = ("A Dance of Fire and Ice",)
+    h3 = "HaeengIn의 불과 얼음의 춤 활동"
+
+    context = {
+        "title": title,
+        "h1": h1,
+        "h3": h3,
+        "repo-link": "https://github.com/HaeengIn/Adofai-Custom-Creates",
+    }
+
+    return templates.TemplateResponse(
+        request=request, context=context, name="adofai/index.html"
+    )
+
+
+@adofai_router.get("/custom")
+async def custom(request: Request):
+    title = "커스텀 레벨 다운로드 - NaGNae"
+    h1 = "커스텀 레벨 다운로드"
+    h3 = "아래의 모든 레벨들의 저작권은 저(HaeengIn)과 공동작업자에게 있습니다."
+
+    context = {"title": title, "h1": h1, "h3": h3, "items": data}
+
+    return templates.TemplateResponse(
+        request=request, context=context, name="adofai/custom/index.html"
+    )
