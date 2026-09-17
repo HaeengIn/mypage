00ce5eaf54f41de02f3bd0effa3ec1ffb890ea3b
HaeengIn <haeengin@gmail.com>
2026-09-17 07:18:33 +0000

    Add auto canonical

--- templates_config.py
+++ templates_config.py
@@ -1,7 +1,18 @@
-from fastapi.templating import Jinja2Templates
 import os
 
-templates = Jinja2Templates(directory="templates")
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+
+def canonical_url_processor(request: Request):
+    base_url = str(request.base_url).rstrip("/")
+    return {"canonical_url": f"{base_url}{request.url.path}"}
+
+
+templates = Jinja2Templates(
+    directory="templates",
+    context_processors=[canonical_url_processor],
+)
 
 
 def static_version(path):
